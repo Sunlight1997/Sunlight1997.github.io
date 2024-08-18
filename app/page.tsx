c8883c05d653c0d92c0ff0ea9e9ab79f9a49bf2a
Sunlight1997 <one.191193@gmail.com>
2024-08-18 08:13:57 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+export default async function LandingPage() {
+  return (
+    <main className="flex bg-black h-full flex-col items-center justify-center">
+    </main>
+  );
+}
