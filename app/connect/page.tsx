bbfde98345b08c6b3b30df101fc3f55a221a671c
Sunlight1997 <one.191193@gmail.com>
2024-09-08 22:01:46 +0200

    Fix: TypeError of Page.tsx

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -3,16 +3,13 @@ import { PersonalDetails } from 'shared/utils/types';
 import { Footer, Loader, Navbar } from 'app/components/ui';
 import dynamic from 'next/dynamic';
 
-type Props = {
-  personalDetails: PersonalDetails;
-};
 
 const Contact = dynamic(() => import('app/components/pages/Connect'), {
   ssr: false,
   loading: () => <Loader />
 });
 
-const ConnectPage = ({ personalDetails }: Props): JSX.Element => {
+const ConnectPage = (): JSX.Element => {
   return (
     <>
         <Contact />
